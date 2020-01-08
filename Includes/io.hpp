450fb58ce182e9d62994ad0891c37d19fc6a9604
Anzo <antonr@live.dk>
2020-01-08 08:46:38 +0100

    Fixed headers

--- Includes/io.hpp
+++ Includes/io.hpp
@@ -1,28 +1,31 @@
 #ifndef IO_H
 #define IO_H
 #include <iostream>
-class io
-{
-    public:
-    io()
-    {
-
-    }
-    ~io()
-    {
-
-    }
-    std::string readCfg();
-
-};
+#include <vector>
 class logger
 {
     public:
         logger();
         ~logger(){};
         void log(const char* input);
+        void log(std::string input);
     private:
         std::string logDir = "logs";
         std::string sessionName;
+};
+class io
+{
+    public:
+    io(logger* l)
+    {
+        Logger = l;
+    }
+    ~io()
+    {
+
+    }
+    std::vector<std::string> readCfg(std::string root);
+    private:
+    logger* Logger;
 };
 #endif
