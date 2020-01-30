0ca873697e7c2b06ef56571b7b6d898424bdbcb1
AnzoDK <antonr@live.dk>
2020-01-30 12:33:30 +0100

    Created a script to get the newest build program

---
+++ getBuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+path="$(pwd)"
+echo working path is $path
+cd ..
+rm -r -f quick-cpp-builder
+git clone --recursive https://github.com/stuckedstudio/quick-cpp-builder.git
+cd quick-cpp-builder
+chmod +x setup.sh
+./setup.sh
+cd $path
+cp ../quick-cpp-builder/build.o build.o
