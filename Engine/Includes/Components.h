e872fede58b5c932a51a7690e6ebd31302e2cb0d
AnzoDK <antonr@live.dk>
2020-01-30 12:32:09 +0100

    Added Components.h for creating new components

---
+++ Engine/Includes/Components.h
@@ -0,0 +1,31 @@
+//This document contains comments, because I'm making this game as a learning process, and therefor needs to remember a few things XD
+
+#pragma once
+#include "ECS.h"
+
+class PositionComponent : public Component
+{
+    private:
+        int xpos,ypos;
+
+    public:
+        int x() {return xpos;}
+        int y() {return ypos;}
+
+        //Using override to override the virtual functions in ECS.h
+        void init() override
+        {
+            xpos = 0;
+            ypos = 0;
+        }
+        void update() override
+        {
+            //We add update logic for this component here
+        }
+        void setPos(int newX, int newY)
+        {
+            xpos = newX;
+            ypos = newY;
+        }
+
+};
